b386fb1474c326c5a44a5eb6cd60d5d7cdf7c913
dashasch708 <dashacomely148@gmail.com>
2022-06-14 23:37:46 +0300

    🙈.gitignore+fetch

--- formData.js
+++ formData.js
@@ -1,28 +1,43 @@
 const f = document.getElementById('opros')
 
-f.onsubmit = function getData(e) {
+f.onsubmit = async function getData(e) {
   e.preventDefault()
-  const name = document.getElementById('name').value
-  const age = document.getElementById('age').value
-  const mark = document.getElementById('mark').value
-  const email = document.getElementById('email').value
-  const yes = document.getElementById('yes')
-  let ch = yes.checked ? 'Да' : 'Нет'
+  const url = 'http://localhost:3000/test'
+  let formData = {
+    name: document.getElementById('name').value,
+    age: document.getElementById('age').value,
+    mark: document.getElementById('mark').value,
+    email: document.getElementById('email').value,
+    yes: document.getElementById('yes').checked
+      ? (ch = 'Да')
+      : (сh = 'Нет'),
+  }
 
-  alert(
-    'Ваше имя: ' +
-      name +
-      '\n' +
-      'Ваш возраст: ' +
-      age +
-      '\n' +
-      'Ваша оценка: ' +
-      mark +
-      '\n' +
-      'Ваш email: ' +
-      email +
-      '\n' +
-      'Ваш ответ: ' +
-      ch
-  )
+  // alert(
+  //   'Ваше имя: ' +
+  //     name +
+  //     '\n' +
+  //     'Ваш возраст: ' +
+  //     age +
+  //     '\n' +
+  //     'Ваша оценка: ' +
+  //     mark +
+  //     '\n' +
+  //     'Ваш email: ' +
+  //     email +
+  //     '\n' +
+  //     'Ваш ответ: ' +
+  //     ch
+  // )
+
+  let response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+
+  let result = await response.json()
+  alert(JSON.stringify(result))
 }
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+app.use(cors())
 
-app.use(express.static('./public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('./'))
 
 app.get('/', (req, res) => {
-  res.sendFile('C:/Users/student/Desktop/project/dasha/index.html')
-  const user_id = req.query.user_id
-  const token = req.query.token
-  const geo = req.qeury.geo
-
-  res.send({
-    user_id: user_id,
-    token: token,
-    geo: geo,
-  })
+  res.sendFile('/Users/dasha_sch/Desktop/website1/index.html')
 })
 
 app.get('/test', (req, res) => {
-  console.log(req.query)
-  res.send(`Жди спасм на ${req.query.email}`)
+  console.log(req.body)
+  res.send(`Жди спасм на ${req.body.email}`)
+
+  res.send(JSON.stringify(req.body.mark))
 })
 
 app.listen(port, () => {
